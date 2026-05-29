2d7e53db8ead97d700cbf4bbd4733ccb561b91d1
Daniel Vlad <danielcvlad@users.noreply.github.com>
2026-05-29 10:18:17 +0000

    fix base path and resume URL for v2 deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/dvlad-devops-lp',
+  base: '/dvlad-devops-lp-v2',
   integrations: [tailwind()],
   preview: {
     allowedHosts: true
